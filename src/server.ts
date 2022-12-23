8b57325d443e16b871b2a9ec31efcf9bd3ea0984
Muhammad Mustafa <muhamed2112@gmail.com>
2022-12-23 23:06:58 +0200

    add routers

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from "express";
-// import bodyParser from 'body-parser'
 import dotenv from "dotenv";
-import { client } from "./database";
+import { routers } from "./routes/api/index.routers";
 
 dotenv.config();
 
@@ -13,6 +12,8 @@ app.use(express.json());
 app.get("/", function (req: Request, res: Response) {
   res.send("Hello World!");
 });
+
+app.use("/api", routers);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
